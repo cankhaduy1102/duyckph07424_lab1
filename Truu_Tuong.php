1b24a53e2895ccb5fde92ce4e1f23637213fdcd2
cankhaduy1102 <62109879+cankhaduy1102@users.noreply.github.com>
2020-04-21 22:55:40 +0700

    Create Truu_Tuong.php

---
+++ Truu_Tuong.php
@@ -0,0 +1,24 @@
+<?php
+	class Khachhang{
+		var $ma_khachhang;
+		var $phonenumber;
+		var $email;
+		var $address;
+
+		function dat_hang(){}
+		function timkiem(){}
+		function thanh_toan(){}
+	}
+
+	class ctv{
+		var $name;
+		var $birthdate;
+		var $cmnd;
+		var $avatar;
+
+		function post(){}
+		function editPost(){}
+		function replyComment(){}
+	}
+
+?>
