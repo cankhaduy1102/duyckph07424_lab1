75bf4665ed59f7994cbbdc6fed02fd988d57b9c0
cankhaduy1102 <62109879+cankhaduy1102@users.noreply.github.com>
2020-04-24 07:54:12 +0700

    MVC

---
+++ mvc/app/controllers/HomeController.php
@@ -0,0 +1,21 @@
+<?php
+
+require_once './app/models/Product.php';
+class HomeController{
+
+    public function index(){
+        $products = Product::all();
+        include_once './app/views/homepage.php';
+    }
+
+    public function detail(){
+        return "Trang chi tiết 1 sản phẩm";
+    }
+
+    public function contact(){
+        return "trang liên hệ của dự án ";
+    }
+
+}
+
+?>
