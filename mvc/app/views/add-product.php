9530de54cb3176c4b680d06c4a106ebee5df2b2a
cankhaduy1102 <62109879+cankhaduy1102@users.noreply.github.com>
2020-04-24 21:14:14 +0700

    Create add-product.php

---
+++ mvc/app/views/add-product.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Thêm sản phẩm</title>
+	 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+</head>
+<body>
+		<form>
+			<div class="form-group">
+	    <label for="">Tên sản phẩm</label>
+	    <input type="text" class="form-control" id="product" aria-describedby="" placeholder="tên sản phẩm">
+	  </div>
+	  <div class="form-group">
+	    <label for="">ảnh sản phẩm</label>
+	    <input type="link" class="form-control" id="product" aria-describedby="" placeholder="ảnh sản phẩm">
+	  </div>
+	  <div class="form-group">
+	    <label for="">giá sản phẩm</label>
+	    <input type="number" class="form-control" id="product" aria-describedby="" placeholder="giá sản phẩm">
+	  </div>
+	  <button type="submit" class="btn btn-primary">Submit</button>
+		</form>
+</body>
+</html>
