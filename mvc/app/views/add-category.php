f22138edcbbd8c867bbb5080a7d5a6a596fd0b1f
cankhaduy1102 <62109879+cankhaduy1102@users.noreply.github.com>
2020-04-24 21:14:11 +0700

    Create add-category.php

---
+++ mvc/app/views/add-category.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Thêm danh mục</title>
+	 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+</head>
+<body>
+	<form action="">
+		<div class="form-group">
+	    <label for="">Danh mục</label>
+	    <input type="text" class="form-control" id="category" aria-describedby="" placeholder="enter category">
+	  </div>
+	  <button type="submit" class="btn btn-primary">Submit</button>
+	</form>
+</body>
+</html>
